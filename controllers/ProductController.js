c2a185425ba3b29f0246813de994ca75aaf8072e
Ederson Franzen Fagundes <edersonff_@hotmail.com.br>
2022-05-12 15:27:04 -0300

    feat: 🎸 Store, update and destroy product changed
    The product controller was changed. Store, update were added and store
    updated. It's now using raw find and it's returning error with 404
    status if product doesn't exists.

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,10 +1,17 @@
 const Product = require("../models/Product");
+const Stock = require("../models/Stock");
 
 module.exports = class productRoutes {
   static async index(req, res) {
     try {
       // get all products
-      const products = Product.findAll();
+      const products = await Product.findAll();
+
+      // verify if product exists
+      if(!products){
+        return res.json({ msg: "Nenhum produto encontrado!" }).status(404);
+      }
+      
       //send data
       res.json({ data: products });
       //
@@ -12,12 +19,19 @@ module.exports = class productRoutes {
       res.json({ msg: err }).status(500);
     }
   }
+
   static async show(req, res) {
     const { id } = req.params;
 
     try {
       // get the product by id
-      const product = Product.findOne({ id });
+      const product = await Product.findOne({where:{id}, raw:true});
+
+      // verify if product exists
+      if(!product){
+        return res.json({ msg: "Produto não encontrado!" }).status(404);
+      }
+
       //send data
       res.json({ data: product });
       //
@@ -25,20 +39,69 @@ module.exports = class productRoutes {
       res.json({ msg: err }).status(500);
     }
   }
+
   static async store(req, res) {
-    const { code, name, description, value } = req.body;
-
     try {
-      // get the product by id
-      const product = Product.findOne({ id });
-      //send data
-      res.json({ data: product });
-      //
+      // store, save product
+      const product = await Product.create(req.body);
+
+      //create stock
+      await Stock.create({ProductId: product.id});
+
+      //send new product data
+      res.json({ data: product }).status(201);
+
     } catch (err) {
       res.json({ msg: err }).status(500);
     }
-
   }
-  static async update() {}
-  static async destroy() {}
+
+  static async update(req, res) {
+    const {id} = req.params;
+    try {
+      // get product by id
+      const product = await Product.findOne({where:{id}});
+
+      // verify if product exists
+      if(!product){
+        return res.json({ msg: "Produto não encontrado!" }).status(404);
+      }
+    
+      // get all body keys and update the product
+      Object.keys(req.body).map((key)=>{
+        product[key] = req.body[key];
+      })
+
+      // update, save product
+      await product.save();
+
+      //send new product data
+      res.json({ msg: "Produto atualizado!", data: product })
+      
+    } catch (err) {
+      res.json({ msg: err }).status(500);
+    }
+  }
+
+  static async destroy(req,res) {
+    const {id} = req.params;
+    try {
+      // get product by id
+      const product = await Product.findOne({where:{id}});
+
+      // verify if product exists
+      if(!product){
+        return res.json({ msg: "Produto não encontrado!" }).status(404);
+      }
+
+      // delete the product
+      await product.destroy();
+
+      res.json({ msg: "Produto excluido!", data: product })
+
+    } catch (err) {
+      res.json({ msg: err }).status(500);
+    }
+  }
+
 };
